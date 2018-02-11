ae9840604531c21cea4442b725883c9f92e6ce88
anysong <985774505@qq.com>
2018-02-11 10:55:06 +0800

    dev

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -2,11 +2,11 @@ angular.module('zc').run(['$rootScope', function($rootScope){
 	/** 执行初始化 **/
 	console.log('run init');
 
-	/** 路由监听器 **/
-	$rootScope.$on("$stateChangeStart", function(){
-		console.log('2',arguments);
+	/** 路由监听器 方法没了 **/
+	// $rootScope.$on("$stateChangeStart", function(){
+	// 	console.log('2',arguments);
 		
-	})
+	// })
 
 	/** 清除所有弹层 **/
 
