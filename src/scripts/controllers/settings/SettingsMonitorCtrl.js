2c7e5421d71949ac877dca59be8dd8a0698a67de
anysong <985774505@qq.com>
2018-02-22 17:25:22 +0800

    dev

--- src/scripts/controllers/settings/SettingsMonitorCtrl.js
+++ src/scripts/controllers/settings/SettingsMonitorCtrl.js
@@ -1,11 +1,34 @@
 angular.module("zc").controller("SettingsMonitorCtrl", ['$scope',function($scope){
 	console.log('SettingsMonitorCtrl');
 
+	var initConfig = function(){
+		$scope.typeList = [
+			{
+				typeName: '地区统计',
+				typeValue: '001'
+			},
+			{
+				typeName: '通话统计',
+				typeValue: '002'
+			}
+		];
+	};
 	var initData = function(){
 		$scope.oLayout = {
 			'number': 2,
-			'list': [null,null]
+			'list': [{
+				'typeName': '请选择分类',   
+				'typeValue': null,        
+				'options': [],            
+				'choicedOpt': '全部'		  
+			},{
+				'typeName': '请选择分类',   
+				'typeValue': null,        
+				'options': [],            
+				'choicedOpt': '全部'		  
+			}]
 		};
+		
 	};
 
 	var selectLayout = function(number){
@@ -15,16 +38,49 @@ angular.module("zc").controller("SettingsMonitorCtrl", ['$scope',function($scope
 
 		for(var i=0;i<number;i++){
 			$scope.oLayout.list.push({
-				'type': null,
-				'params': null
-			})
+				'typeName': '请选择分类',   //分类名称
+				'typeValue': null,        //分类ID
+				'options': [],            //选项
+				'choicedOpt': '全部'		  //选中项
+			});
 		};
 		console.log($scope.oLayout.list.length)
 	};
+	var getOptions = function(typeValue){
+		var arr = [];
+		switch (typeValue){
+			case '001':
+				arr = ['北京','上海','杭州'];
+				break;
+			case '002':
+				arr = ['xini','ni','xx'];
+				break;
+			default:
+		};
+
+		return arr;
+	};
+	var setType = function(type, parentIndex, index){
+		console.log(type.typeName);
+		// console.log(index);
+		$scope.oLayout.list[parentIndex].typeName = type.typeName;
+		$scope.oLayout.list[parentIndex].typeValue = type.typeValue;
+
+		//获取不同分类的下拉子选项
+		$scope.oLayout.list[parentIndex].choicedOpt = '全部';
+		$scope.oLayout.list[parentIndex].options = getOptions(type.typeValue);
+
+	};
+	var setOptions = function(){
+
+	};
 	var initFunc = function(){
 		$scope.selectLayout = selectLayout;
+		$scope.setType = setType;
+		$scope.setOptions = setOptions;
 	};
 	var init = function(){
+		initConfig();
 		initData();
 		initFunc();
 	};
--- src/tar/controllers.js
+++ src/tar/controllers.js
@@ -45,42 +45,6 @@ angular.module("zc").controller("MainCtrl", ['$scope','$state','$rootScope','Cha
 
 	init();
 }])
-angular.module("zc").controller("SettingsCtrl",[function(){
-	console.log('SettingsCtrl');
-}]);
-angular.module("zc").controller("SettingsMonitorCtrl", ['$scope',function($scope){
-	console.log('SettingsMonitorCtrl');
-
-	var initData = function(){
-		$scope.oLayout = {
-			'number': 2,
-			'list': [null,null]
-		};
-	};
-
-	var selectLayout = function(number){
-		//初始化；
-		$scope.oLayout.number = number;
-		$scope.oLayout.list = [];
-
-		for(var i=0;i<number;i++){
-			$scope.oLayout.list.push({
-				'type': null,
-				'params': null
-			})
-		};
-		console.log($scope.oLayout.list.length)
-	};
-	var initFunc = function(){
-		$scope.selectLayout = selectLayout;
-	};
-	var init = function(){
-		initData();
-		initFunc();
-	};
-
-	init();
-}])
 angular.module("zc").controller("HomeCtrl", [function(){
 	console.log('HomeCtrl');
 }]);
@@ -108,3 +72,95 @@ angular.module("zc").controller("HomeMonitorCtrl", ['$scope',function($scope){
 	];
 
 }]);
+angular.module("zc").controller("SettingsCtrl",[function(){
+	console.log('SettingsCtrl');
+}]);
+angular.module("zc").controller("SettingsMonitorCtrl", ['$scope',function($scope){
+	console.log('SettingsMonitorCtrl');
+
+	var initConfig = function(){
+		$scope.typeList = [
+			{
+				typeName: '地区统计',
+				typeValue: '001'
+			},
+			{
+				typeName: '通话统计',
+				typeValue: '002'
+			}
+		];
+	};
+	var initData = function(){
+		$scope.oLayout = {
+			'number': 2,
+			'list': [{
+				'typeName': '请选择分类',   
+				'typeValue': null,        
+				'options': [],            
+				'choicedOpt': '全部'		  
+			},{
+				'typeName': '请选择分类',   
+				'typeValue': null,        
+				'options': [],            
+				'choicedOpt': '全部'		  
+			}]
+		};
+		
+	};
+
+	var selectLayout = function(number){
+		//初始化；
+		$scope.oLayout.number = number;
+		$scope.oLayout.list = [];
+
+		for(var i=0;i<number;i++){
+			$scope.oLayout.list.push({
+				'typeName': '请选择分类',   //分类名称
+				'typeValue': null,        //分类ID
+				'options': [],            //选项
+				'choicedOpt': '全部'		  //选中项
+			});
+		};
+		console.log($scope.oLayout.list.length)
+	};
+	var getOptions = function(typeValue){
+		var arr = [];
+		switch (typeValue){
+			case '001':
+				arr = ['北京','上海','杭州'];
+				break;
+			case '002':
+				arr = ['xini','ni','xx'];
+				break;
+			default:
+		};
+
+		return arr;
+	};
+	var setType = function(type, parentIndex, index){
+		console.log(type.typeName);
+		// console.log(index);
+		$scope.oLayout.list[parentIndex].typeName = type.typeName;
+		$scope.oLayout.list[parentIndex].typeValue = type.typeValue;
+
+		//获取不同分类的下拉子选项
+		$scope.oLayout.list[parentIndex].choicedOpt = '全部';
+		$scope.oLayout.list[parentIndex].options = getOptions(type.typeValue);
+
+	};
+	var setOptions = function(){
+
+	};
+	var initFunc = function(){
+		$scope.selectLayout = selectLayout;
+		$scope.setType = setType;
+		$scope.setOptions = setOptions;
+	};
+	var init = function(){
+		initConfig();
+		initData();
+		initFunc();
+	};
+
+	init();
+}])
