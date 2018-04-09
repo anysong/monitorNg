bf1f67d87cd8cb70cc7557246f1c49b4826f52cd
anysong <985774505@qq.com>
2018-04-10 01:12:58 +0800

    dev

--- src/scripts/controllers/MainCtrl.js
+++ src/scripts/controllers/MainCtrl.js
@@ -1,9 +1,9 @@
-angular.module("zc").controller("MainCtrl", ['$scope','$state','$rootScope','ChatServ','CallServ',
-	function($scope,$state,$rootScope,ChatServ,CallServ){
+angular.module("zc").controller("MainCtrl", ['$scope','$state',
+	function($scope,$state){
 
 
 	var initFunc = function(){
-	
+
 	};
 	var init = function(){
 		initFunc();
